f4f51191ce87e5189bbf5d5c5a7240ac7c50cd24
guilhermeluizcosta <guilhermelc10@gmail.com>
2022-10-03 17:35:54 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o numero:");
+        int numero = entrada.nextInt();
+        System.out.println("O numero informado foi: " + numero);
+        
+        
+    }
+}
